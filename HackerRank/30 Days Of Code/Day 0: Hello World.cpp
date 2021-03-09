518a8dbc5dadef02b1870d5d7bf6fa40e6b6ca57
kunal077 <79495351+kunal077@users.noreply.github.com>
2021-03-09 20:19:25 +0530

    Create Day 0: Hello World.cpp

---
+++ HackerRank/30 Days Of Code/Day 0: Hello World.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+using namespace std;
+
+int main()
+
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string inputString;
+    getline(cin, inputString);
+
+    cout<<"Hello, World.\n";
+    cout<<inputString;
+ 
+
+    return 0;
+
+} 
